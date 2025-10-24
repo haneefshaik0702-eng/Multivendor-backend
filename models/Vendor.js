49cea334e6f8e2ea38453f0d4bf4dfa34c829635
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-10-24 09:24:06 +0530

    Update Vendor.js

--- models/Vendor.js
+++ models/Vendor.js
@@ -1,1 +1,11 @@
+// models/Vendor.js
+import mongoose from "mongoose";
+
+const vendorSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true } // later hash this with bcrypt
+}, { timestamps: true });
+
+export default mongoose.model("Vendor", vendorSchema);
 
