7505099d966232536b5f27549fed0a0289ef6f70
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-10-24 09:55:45 +0530

    Update vendorRoutes.js

--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -1,19 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-// Example: register vendor
-router.post("/register", (req, res) => {
-  res.send("Vendor registered successfully!");
-});
-
-// Example: login vendor
-router.post("/login", (req, res) => {
-  res.send("Vendor logged in successfully!");
-});
-
-// Example: list all vendors
+// Sample route
 router.get("/", (req, res) => {
-  res.send("List of all vendors");
+  res.send("Vendor routes working ✅");
 });
 
 module.exports = router;
