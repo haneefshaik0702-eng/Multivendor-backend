af835d1ab96afdd47783676f9afd8ae5ec4d91ae
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-10-24 09:30:21 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,1 +1,13 @@
+// models/Product.js
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  vendorId: { type: mongoose.Schema.Types.ObjectId, ref: "Vendor", required: true },
+  description: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model("Product", productSchema);
 
