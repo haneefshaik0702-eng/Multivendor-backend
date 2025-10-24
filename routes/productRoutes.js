3dfd9044e65e54e72954301fe494d5754f467665
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-10-24 09:34:49 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,1 +1,43 @@
+// routes/productRoutes.js
+import express from "express";
+import Product from "../models/Product.js";
+
+const router = express.Router();
+
+// Create product
+router.post("/", async (req, res) => {
+  try {
+    const product = new Product(req.body);
+    await product.save();
+    res.status(201).json(product);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// List all
+router.get("/", async (req, res) => {
+  const products = await Product.find().populate("vendorId", "name email");
+  res.json(products);
+});
+
+// Get one
+router.get("/:id", async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  res.json(product);
+});
+
+// Update
+router.put("/:id", async (req, res) => {
+  const updated = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updated);
+});
+
+// Delete
+router.delete("/:id", async (req, res) => {
+  await Product.findByIdAndDelete(req.params.id);
+  res.json({ message: "Product deleted" });
+});
+
+export default router;
 
