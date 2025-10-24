c4a439d0bba56aa944e623a3d167b9a99960cb0e
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-10-24 09:46:27 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,43 +1,24 @@
-// routes/productRoutes.js
-import express from "express";
-import Product from "../models/Product.js";
-
+const express = require("express");
 const router = express.Router();
 
-// Create product
-router.post("/", async (req, res) => {
-  try {
-    const product = new Product(req.body);
-    await product.save();
-    res.status(201).json(product);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
+// Add a product
+router.post("/add", (req, res) => {
+  res.send("Product added successfully!");
 });
 
-// List all
-router.get("/", async (req, res) => {
-  const products = await Product.find().populate("vendorId", "name email");
-  res.json(products);
+// Edit a product
+router.put("/edit/:id", (req, res) => {
+  res.send(`Product ${req.params.id} updated successfully!`);
 });
 
-// Get one
-router.get("/:id", async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  res.json(product);
+// Delete a product
+router.delete("/delete/:id", (req, res) => {
+  res.send(`Product ${req.params.id} deleted successfully!`);
 });
 
-// Update
-router.put("/:id", async (req, res) => {
-  const updated = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  res.json(updated);
+// List all products
+router.get("/", (req, res) => {
+  res.send("List of all products");
 });
 
-// Delete
-router.delete("/:id", async (req, res) => {
-  await Product.findByIdAndDelete(req.params.id);
-  res.json({ message: "Product deleted" });
-});
-
-export default router;
-
+module.exports = router;
