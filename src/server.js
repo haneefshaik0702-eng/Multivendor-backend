dbcb39b4b0096327f49859d4fc026e43ecd44765
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-10-24 10:01:22 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+// Import routes
+const vendorRoutes = require("./routes/vendorRoutes");
+app.use("/api/vendors", vendorRoutes);
+
+const PORT = process.env.PORT || 10000;
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
 
