f627e2e7dd8bdf32257f065c7949b14fa7608dcf
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-10-24 09:37:17 +0530

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,1 +1,15 @@
+// models/Order.js
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+  customerName: { type: String, required: true },
+  products: [{
+    productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+    quantity: { type: Number, default: 1 }
+  }],
+  totalAmount: { type: Number, default: 0 },
+  status: { type: String, default: "Pending" }
+}, { timestamps: true });
+
+export default mongoose.model("Order", orderSchema);
 
