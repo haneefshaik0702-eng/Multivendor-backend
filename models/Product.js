004e02a04b674838ba0081c8212c26416bdc4097
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-10-24 09:52:18 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,13 +1,14 @@
-// models/Product.js
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
+  vendorId: { type: mongoose.Schema.Types.ObjectId, ref: "Vendor", required: true },
   name: { type: String, required: true },
   price: { type: Number, required: true },
-  vendorId: { type: mongoose.Schema.Types.ObjectId, ref: "Vendor", required: true },
-  description: String,
-  createdAt: { type: Date, default: Date.now }
+  category: { type: String },
+  stock: { type: Number, default: 0 },
+  image: { type: String },
+  description: { type: String },
+  createdAt: { type: Date, default: Date.now },
 });
 
-export default mongoose.model("Product", productSchema);
-
+module.exports = mongoose.model("Product", productSchema);
