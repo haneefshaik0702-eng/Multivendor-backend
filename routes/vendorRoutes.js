7c187130881fa3f8076493490c40eeae084f9aa8
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-10-24 10:13:22 +0530

    Delete routes directory

--- routes/vendorRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Sample route
-router.get("/", (req, res) => {
-  res.send("Vendor routes working ✅");
-});
-
-module.exports = router;
